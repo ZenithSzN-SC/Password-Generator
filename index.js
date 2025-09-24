46a0fa156cf82491de7399eec6de25498a9296cc
Stanley chapalapata <stanleymxt@icloud.com>
2025-09-24 21:44:50 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let randomPass = ""
+
+let genPass1El = document.getElementById("genPass1")
+let genPass2El = document.getElementById("genPass2")
+
+function randomIndex(){
+            return Math.floor(Math.random() * characters.length)
+        }
+function generatePass(){
+    for (let i = 0; i < characters.length; i++){
+        
+        randomPass += characters[randomIndex()]
+
+        genPass1El.textContent = randomPass
+    }
+}
